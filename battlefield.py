54262f65d4c7dcb6d13f3088396afd5b8273a357
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-07-18 16:35:06 -0400

    Program debugged. Core program works as intended. Will look towards extra credit at later date.

--- battlefield.py
+++ battlefield.py
@@ -8,16 +8,43 @@ class Battlefield:
     def run_game(self):
         self.display_welcome()
         d1=Dinosaur("Troodon",15,50)
-        r1=Robot("Buzz",50)
+        r1=Robot("Buzz",50,"Laser",16)
+        self.robot_wins=True
+        self.robot_wins=self.battle_phase(d1,r1)
+        self.display_winner(self.robot_wins,d1,r1)
+
+
+
+
 
 
     def display_welcome(self):
         print("Welcome to Robot vs Dinosaur!")
 
-    def battle_phase(self):
-        pass
+    def battle_phase(self,d1,r1):
+        while (d1.health>0 and r1.health>0):
+            print(f"{r1.name} HP: {r1.health}")
+            print(f"{d1.name} HP: {d1.health}")
+            print(f"{r1.name} attacks with his {r1.weapons[0].name} and deals {r1.weapons[0].attack_power} damage")
+            d1.health-=r1.weapons[0].attack_power
+            if (d1.health >0):
+                print(f"{d1.name} attacks and deals {d1.attack_power} damage!")
+                r1.health-=d1.attack_power
+            else:
+                pass
+        
+        if (r1.health<0):
+            return False
+        else:
+            return True
 
-    def display_winner(self):
-        print("winner")
+            
+
+
+    def display_winner(self,robot_wins,d1,r1):
+        if (robot_wins == True):
+            print(f"{r1.name} has won the fight!")
+        else:
+            print(f"{d1.name} has won the fight!")
 
     
--- robot.py
+++ robot.py
@@ -5,11 +5,11 @@ class Robot:
         self.name=name
         self.health=health
         self.weapons=[]
-        self.add_weapon(self.weapon_name,self.attack_power)
+        self.add_weapon(weapon_name,attack_power)
 
 
     def attack(self,dinosaur):
         pass
     def add_weapon(self,name,attack_power):
-        w1=Weapon(self.name,self.attack_power)
+        w1=Weapon(name,attack_power)
         self.weapons.append(w1)
