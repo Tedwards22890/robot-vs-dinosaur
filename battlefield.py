a2c98e1916f1a535aa8e7557f0a9f10b79b93170
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-07-19 13:29:20 -0400

    Added line breaks for ease of reading. Added delays (time.sleep) so player is not overwhelmed with text at once. Heading texted added and organized to ensure readability as well.

--- battlefield.py
+++ battlefield.py
@@ -14,25 +14,32 @@ class Battlefield:
 
 
 
-
-
-
     def display_welcome(self):
         print("Welcome to Robot vs Dinosaur!")
+        time.sleep(3)
 
     def battle_phase(self):
         fleet1=Fleet()
         herd1=Herd()
         z=0
         while (z==0):
+            print("Your Army:")
             for x in range(len(fleet1.robots)):
                 print(f"{fleet1.robots[x].name}: {fleet1.robots[x].health}hp")
+            print()
+            time.sleep(1)
+            print("Enemy Army:")
             for x in range(len(herd1.dinosaurs)):
                 print(f"{herd1.dinosaurs[x].name}: {herd1.dinosaurs[x].health}hp")
+            print()
+            time.sleep(2)
             print("Which weapon do you want to attack with?\n0)Laser\n1)Saw\n2)Plasma Beam")
             choice=int(input("Select 0, 1 or 2: "))
             weapon_choice=fleet1.get_weapon_name(choice)
             weapon_damage=fleet1.get_weapon_damage(choice)
+
+            print()
+
 
             print(f"{fleet1.robots[0].name} attacks with its {weapon_choice} and deals {weapon_damage} damage")
             herd1.dinosaurs[0].health-=weapon_damage
@@ -50,6 +57,9 @@ class Battlefield:
             if (len(fleet1.robots) ==0):
                 return False
                 break
+
+            print()
+            print()
 
             time.sleep(3)
             
